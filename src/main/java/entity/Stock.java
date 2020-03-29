56860116ec6a1660414850b365c2a8ddf57a9ffa
Shepico <shepico@gmail.com>
2020-03-29 18:14:05 +0400

    создан class Stock

---
+++ src/main/java/entity/Stock.java
@@ -0,0 +1,30 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "stock")
+public class Stock {
+    @Id
+    @Column(name = "stk_ticker")
+    String ticker;
+
+    @ManyToOne
+    @JoinColumn(name = "stk_esid")
+    int sector;
+
+    @Column(name = "stk_name")
+    String name;
+
+    @Column(name = "stk_date")
+    Date dateCreate;
+
+    public Stock() {
+
+    }
+
+    public String getStock() {
+        return ticker + " - " + name;
+    }
+}
--- src/main/java/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -1,3 +1,6 @@
+package utils;
+
+import entity.Stock;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -11,7 +14,7 @@ public class HibernateSessionFactoryUtil {
         if (sessionFactory == null) {
             try {
                 Configuration configuration = new Configuration().configure();
-                //configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(Stock.class);
                 //configuration.addAnnotatedClass(Auto.class);
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
                 sessionFactory = configuration.buildSessionFactory(builder.build());
