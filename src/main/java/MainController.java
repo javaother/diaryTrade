45488609f6d4b42718bec7240cb6864a1b7f54f3
Shepico <shepico@gmail.com>
2020-03-29 16:56:19 +0400

    onAction for Button

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -4,7 +4,32 @@ public class MainController {
 
     }
 
-    public void closeApp() {
+    /*public void closeApp() {
         System.out.println("Button exit!");
+
+    }*/
+
+    public void openDirIndeces() {
+        System.out.println("Button open INDICES");
+    }
+
+    public void openDirStock() {
+        System.out.println("Button open STOCK");
+    }
+
+    public void operQUIK() {
+        System.out.println("Button QUIK");
+    }
+
+    public void docOpen() {
+        System.out.println("Button OPEN DOC");
+    }
+
+    public void docClose() {
+        System.out.println("Button CLOSE DOC");
+    }
+
+    public void reportPriceChange() {
+        System.out.println("Button REPORT PRICE CHANGE");
     }
 }
