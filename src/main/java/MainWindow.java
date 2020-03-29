5e27b3a70d73f801f5e863afabfc65a36deb28b7
Shepico <shepico@gmail.com>
2020-03-29 16:00:02 +0400

    create FXML main, связь с app

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainWindow extends Application {
 
@@ -9,6 +15,20 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/mainScene.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace(); //todo обработка ошибки main
+        }
+
+        if (root != null) {
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }
 
     }
 }
